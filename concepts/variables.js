9a1088f839d499a70f3fb7f3d60485d21d742949
EsmeMP <esmeralda.padilla.com@gmail.com>
2024-09-12 20:33:48 -0600

    03 Variables let y const

---
+++ concepts/variables.js
@@ -0,0 +1,26 @@
+// Declaracion de variables con let y const
+// var -- en desuso
+
+
+// Variables let; Cambian de valores
+// declaracion
+let age;
+console.log(age);
+// inicializacion
+age = 23;
+console.log(age);
+
+
+// declaracion e inicializacion
+let name = "Francisco";
+console.log(name);
+name = "Paco";
+console.log(name);
+
+
+// constantes const; no cambairan de valor
+// Siempre deben de ir inicializadas
+const PI = 3.1416;
+// PI = 3.15;
+console.log(PI);
+
