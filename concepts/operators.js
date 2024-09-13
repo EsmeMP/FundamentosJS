dddb2c53b26e42014fa87af524101e20139d008f
EsmeMP <esmeralda.padilla.com@gmail.com>
2024-09-12 20:28:55 -0600

    02 Operadores

---
+++ concepts/operators.js
@@ -0,0 +1,46 @@
+// Operadores Matematicos
+// + y -
+// * y /
+// % y **
+
+
+console.log (2 + 2);
+console.log (2 - 2);
+console.log (2 * 2);
+console.log (2 / 2);
+console.log (2 % 2);
+console.log (2 ** 2);
+
+
+// Operadores de asignacion
+let a = 10;
+// atajo a = a + 5
+a += 5;
+a -= 5;
+a *= 5;
+a /= 5;
+a %= 5;
+a **= 5;
+
+
+console.log(a);
+
+
+// Operadores de Comparacion
+console.log(2 > 2);
+console.log(2 < 2);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2 === 2);
+console.log(2 == 2);
+console.log(2 != 2);
+console.log(2 !== 2);
+
+
+// Operadores Logicos
+// and
+console.log (true && true);
+// or
+console.log (true || false);
+// not
+console.log (!true);
